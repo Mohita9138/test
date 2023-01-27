e549c0890c9300e75b01628f95f6485db9584d8b
Mr. Robot <90469527+Mohita9138@users.noreply.github.com>
2023-01-28 00:43:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print("passed1")
+import subprocess
+def update():
+    process = subprocess.Popen("git checkout . && git pull ",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    print(process)
+print("pass")
+update()
