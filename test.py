8081f44ccb2d3b9236daaf211f987501cff278b5
Mr. Robot <90469527+Mohita9138@users.noreply.github.com>
2023-01-28 00:49:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import subprocess
 def update():
     process = subprocess.Popen("git checkout . && git pull ",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     (use,nouse)=process.communicate()
-    if "Already up to date" in use:
+    if "Already up to date" in use.decode():
         print("[+] it is updated")
     else:
         print("[+] updating....")
