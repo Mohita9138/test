14bd96ed1569f65671ef82f0e6578d28054db292
Mr. Robot <90469527+Mohita9138@users.noreply.github.com>
2023-01-28 00:46:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ def update():
     process = subprocess.Popen("git checkout . && git pull ",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     (use,nouse)=process.communicate()
     print(use)
-print("pass")
+print("pass1")
 update()
